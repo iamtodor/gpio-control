da9dda9fe8f4eb6bc2abfcebc17b58a629a9f11a
iamtodor <todor.ilya@gmail.com>
2017-03-16 16:12:44 +0200

    add and change layouts for device group and single device header and item

--- app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/screen/portManagement/ExpandableExampleAdapter.java
+++ app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/screen/portManagement/ExpandableExampleAdapter.java
@@ -32,9 +32,14 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ExpandableExampleAdapter
-        extends AbstractExpandableItemAdapter<ExpandableExampleAdapter.MyGroupViewHolder, ExpandableExampleAdapter.MyChildViewHolder> {
+        extends AbstractExpandableItemAdapter<ExpandableExampleAdapter.DeviceGroupHeaderViewHolder, ExpandableExampleAdapter.SingleDeviceItemViewHolder> {
 
     private static final String TAG = ExpandableExampleAdapter.class.getSimpleName();
+    private static final int DEVICE_GROUP_HEADER_VIEW_TYPE = 1;
+    private static final int DEVICE_GROUP_ITEM_VIEW_TYPE = 2;
+
+    private static final int SINGLE_DEVICE_HEADER_VIEW_TYPE = 3;
+    private static final int SINGLE_DEVICE_ITEM_VIEW_TYPE = 4;
 
     private final Context context;
     private List<Header> deviceGroupHeaderList = new ArrayList<>();
@@ -75,7 +80,7 @@ public class ExpandableExampleAdapter
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        if(deviceGroupHeaderList.get(groupPosition) instanceof DeviceGroupHeaderPinManagement) {
+        if (deviceGroupHeaderList.get(groupPosition) instanceof DeviceGroupHeaderPinManagement) {
             final Controller controller = (Controller) deviceGroupHeaderList.get(groupPosition).childAt(childPosition);
             return controller.getId();
         }
@@ -85,30 +90,48 @@ public class ExpandableExampleAdapter
 
     @Override
     public int getGroupItemViewType(int groupPosition) {
-        return 0;
+        if (deviceGroupHeaderList.get(groupPosition) instanceof DeviceGroupHeaderPinManagement) {
+            return DEVICE_GROUP_HEADER_VIEW_TYPE;
+        } else
+            return SINGLE_DEVICE_HEADER_VIEW_TYPE;
     }
 
     @Override
     public int getChildItemViewType(int groupPosition, int childPosition) {
-        return 0;
+        if (deviceGroupHeaderList.get(groupPosition) instanceof DeviceGroupHeaderPinManagement) {
+            return DEVICE_GROUP_ITEM_VIEW_TYPE;
+        } else
+            return SINGLE_DEVICE_ITEM_VIEW_TYPE;
     }
 
     @Override
-    public MyGroupViewHolder onCreateGroupViewHolder(ViewGroup parent, int viewType) {
+    public DeviceGroupHeaderViewHolder onCreateGroupViewHolder(ViewGroup parent, int viewType) {
         final LayoutInflater inflater = LayoutInflater.from(context);
-        final View v = inflater.inflate(R.layout.device_group_item, parent, false);
-        return new MyGroupViewHolder(v, onSelectedGroupListener);
+        if(viewType == DEVICE_GROUP_HEADER_VIEW_TYPE) {
+            final View v = inflater.inflate(R.layout.device_group_header_port_manager, parent, false);
+            return new DeviceGroupHeaderViewHolder(v, onSelectedGroupListener);
+        } else if(viewType == SINGLE_DEVICE_HEADER_VIEW_TYPE) {
+            final View v = inflater.inflate(R.layout.single_device_header_port_manager, parent, false);
+            return new DeviceGroupHeaderViewHolder(v, onSelectedGroupListener);
+        }
+        throw new RuntimeException("No match for onCreateGroupViewHolder" + viewType + ".");
     }
 
     @Override
-    public MyChildViewHolder onCreateChildViewHolder(ViewGroup parent, int viewType) {
+    public SingleDeviceItemViewHolder onCreateChildViewHolder(ViewGroup parent, int viewType) {
         final LayoutInflater inflater = LayoutInflater.from(context);
-        final View v = inflater.inflate(R.layout.device_list_item_port_management, parent, false);
-        return new MyChildViewHolder(v);
+        if(viewType == DEVICE_GROUP_ITEM_VIEW_TYPE) {
+            final View v = inflater.inflate(R.layout.device_group_item_port_manager, parent, false);
+            return new SingleDeviceItemViewHolder(v);
+        } else if(viewType == SINGLE_DEVICE_ITEM_VIEW_TYPE) {
+            final View v = inflater.inflate(R.layout.single_device_item_port_manager, parent, false);
+            return new SingleDeviceItemViewHolder(v);
+        }
+        throw new RuntimeException("No match for onCreateChildViewHolder" + viewType + ".");
     }
 
     @Override
-    public void onBindGroupViewHolder(MyGroupViewHolder holder, int groupPosition, int viewType) {
+    public void onBindGroupViewHolder(DeviceGroupHeaderViewHolder holder, int groupPosition, int viewType) {
         final Header item = deviceGroupHeaderList.get(groupPosition);
 
         holder.name.setText(item.getName());
@@ -128,8 +151,7 @@ public class ExpandableExampleAdapter
     }
 
     @Override
-    public void onBindChildViewHolder(MyChildViewHolder holder, int groupPosition, int childPosition, int viewType) {
-//        final Controller controller = deviceGroupHeaderList.get(groupPosition).childAt(childPosition);
+    public void onBindChildViewHolder(SingleDeviceItemViewHolder holder, int groupPosition, int childPosition, int viewType) {
         if (deviceGroupHeaderList.get(groupPosition) instanceof DeviceHeaderPinManager) {
             final Controller controller = (Controller) deviceGroupHeaderList.get(groupPosition).childAt(childPosition);
             final Drawable drawable = VectorDrawableCompat.create(context.getResources(),
@@ -142,12 +164,26 @@ public class ExpandableExampleAdapter
         }
     }
 
-    static class MyGroupViewHolder extends AbstractExpandableItemViewHolder implements View.OnClickListener {
+    static class BaseHeaderViewHolder extends AbstractExpandableItemViewHolder {
+
+        public BaseHeaderViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    static class BaseItemViewHolder extends AbstractExpandableItemViewHolder {
+
+        public BaseItemViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    static class DeviceGroupHeaderViewHolder extends AbstractExpandableItemViewHolder implements View.OnClickListener {
         @BindView(R.id.selection) CheckBox selection;
         @BindView(R.id.name) TextView name;
         @BindView(R.id.dropped_arrow) ImageView droppedArrow;
 
-        MyGroupViewHolder(View itemView, CompoundButton.OnCheckedChangeListener onSelectedGroupListener) {
+        DeviceGroupHeaderViewHolder(View itemView, CompoundButton.OnCheckedChangeListener onSelectedGroupListener) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             itemView.setOnClickListener(this);
@@ -164,7 +200,83 @@ public class ExpandableExampleAdapter
         }
     }
 
-    static class MyChildViewHolder extends AbstractExpandableItemViewHolder implements View.OnClickListener {
+    static class DeviceGroupItemViewHolder extends AbstractExpandableItemViewHolder implements View.OnClickListener {
+        private final PopupMenu popup;
+
+        @BindView(R.id.selection) CheckBox selection;
+        @BindView(R.id.folder) ImageView folder;
+        @BindView(R.id.name) TextView name;
+        @BindView(R.id.port) TextView port;
+        @BindView(R.id.menu) ImageView imageViewOptions;
+
+        DeviceGroupItemViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+            imageViewOptions.setOnClickListener(this);
+
+            popup = new PopupMenu(itemView.getContext(), imageViewOptions);
+            popup.inflate(R.menu.group_item_popup_menu);
+            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.edit_image:
+                            // TODO: 3/10/17 add edit image
+                            break;
+                        case R.id.edit_name:
+                            // TODO: 3/10/17 add edit name
+                            break;
+                        case R.id.add_to_group:
+                            // TODO: 3/10/17 add add to group
+                            break;
+                        case R.id.remove_from_group:
+                            // TODO: 3/10/17 add remove from group
+                            break;
+                        case R.id.duplicate:
+                            // TODO: 3/10/17 add duplicated
+                            break;
+                        case R.id.ungroup:
+                            // TODO: 3/10/17 add ungroup
+                            break;
+                    }
+                    return false;
+                }
+            });
+        }
+
+        @Override public void onClick(View view) {
+            if (view.getId() == R.id.menu) {
+                popup.show();
+            } else {
+                selection.setChecked(!selection.isChecked());
+            }
+        }
+    }
+
+    static class SingleDeviceHeaderViewHolder extends AbstractExpandableItemViewHolder implements View.OnClickListener {
+        @BindView(R.id.selection) CheckBox selection;
+        @BindView(R.id.name) TextView name;
+        @BindView(R.id.dropped_arrow) ImageView droppedArrow;
+
+        SingleDeviceHeaderViewHolder(View itemView, CompoundButton.OnCheckedChangeListener onSelectedGroupListener) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+            droppedArrow.setOnClickListener(this);
+            selection.setOnCheckedChangeListener(onSelectedGroupListener);
+        }
+
+        @Override public void onClick(View view) {
+            if (view.getId() == R.id.selection) {
+                // TODO: 3/14/17 add hiding and showing devices
+            } else {
+
+            }
+        }
+    }
+
+    static class SingleDeviceItemViewHolder extends AbstractExpandableItemViewHolder implements View.OnClickListener {
         private final PopupMenu popup;
 
         @BindView(R.id.image) ImageView imagePort;
@@ -174,7 +286,7 @@ public class ExpandableExampleAdapter
         @BindView(R.id.switch_active) SwitchCompat switchCompat;
         @BindView(R.id.menu) ImageView imageViewOptions;
 
-        MyChildViewHolder(View itemView) {
+        SingleDeviceItemViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             itemView.setOnClickListener(this);
@@ -211,7 +323,7 @@ public class ExpandableExampleAdapter
     }
 
     @Override
-    public boolean onCheckCanExpandOrCollapseGroup(MyGroupViewHolder holder, int groupPosition, int x, int y, boolean expand) {
+    public boolean onCheckCanExpandOrCollapseGroup(DeviceGroupHeaderViewHolder holder, int groupPosition, int x, int y, boolean expand) {
         return deviceGroupHeaderList.get(groupPosition).childrenCount() > 0;
     }
 }
