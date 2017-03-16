c5a2981400c1acbdd989e692f72a42453d2369be
iamtodor <todor.ilya@gmail.com>
2017-03-16 14:41:09 +0200

    remove unneeded setters

--- app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/model/Controller.java
+++ app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/model/Controller.java
@@ -16,38 +16,23 @@ public class Controller {
         this.imagePortsDrawableId = imagePortsDrawableId;
         this.isActive = isActive;
         this.isSelected = isSelected;
+        this.id = id;
     }
 
     public String getControllerId() {
         return controllerId;
     }
 
-    public void setControllerId(String controllerId) {
-        this.controllerId = controllerId;
-    }
-
     public String getPortName() {
         return portName;
-    }
-
-    public void setPortName(String portName) {
-        this.portName = portName;
     }
 
     public int getImagePortsDrawableId() {
         return imagePortsDrawableId;
     }
 
-    public void setImagePortsDrawableId(int imagePortsDrawableId) {
-        this.imagePortsDrawableId = imagePortsDrawableId;
-    }
-
     public boolean isActive() {
         return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
     }
 
     public boolean isSelected() {
