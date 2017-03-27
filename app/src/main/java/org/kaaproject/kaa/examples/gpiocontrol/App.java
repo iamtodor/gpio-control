791a8607fdc5daf95b486200e17ab4517a58b0ad
iamtodor <todor.ilya@gmail.com>
2017-03-27 21:17:24 +0300

    add leak canary

--- app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/App.java
+++ app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/App.java
@@ -1,6 +1,10 @@
 package org.kaaproject.kaa.examples.gpiocontrol;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import org.kaaproject.kaa.examples.gpiocontrol.utils.PreferencesImpl;
 
@@ -8,9 +12,21 @@ import io.realm.Realm;
 
 public class App extends Application {
 
+    private RefWatcher refWatcher;
+
     @Override public void onCreate() {
         super.onCreate();
         PreferencesImpl.init(getApplicationContext());
         Realm.init(this);
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
+    }
+
+    public static RefWatcher getRefWatcher(Context context) {
+        App application = (App) context.getApplicationContext();
+        return application.refWatcher;
     }
 }
--- app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/screen/base/BaseFragment.java
+++ app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/screen/base/BaseFragment.java
@@ -4,6 +4,10 @@ package org.kaaproject.kaa.examples.gpiocontrol.screen.base;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 
+import com.squareup.leakcanary.RefWatcher;
+
+import org.kaaproject.kaa.examples.gpiocontrol.App;
+
 public class BaseFragment extends Fragment {
 
     protected final String TAG = this.getClass().getSimpleName();
@@ -14,6 +18,12 @@ public class BaseFragment extends Fragment {
 
     protected ActionBar getSupportActionBar() {
         return getBaseActivity().getSupportActionBar();
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        RefWatcher refWatcher = App.getRefWatcher(getActivity());
+        refWatcher.watch(this);
     }
 
 }
