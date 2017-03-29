8a86f9b60e0d10451d0f0d6c189fcf5d71412545
iamtodor <todor.ilya@gmail.com>
2017-03-29 13:28:42 +0300

    remove leak canary

--- app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/screen/base/BaseFragment.java
+++ app/src/main/java/org/kaaproject/kaa/examples/gpiocontrol/screen/base/BaseFragment.java
@@ -4,10 +4,6 @@ package org.kaaproject.kaa.examples.gpiocontrol.screen.base;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 
-import com.squareup.leakcanary.RefWatcher;
-
-import org.kaaproject.kaa.examples.gpiocontrol.App;
-
 public class BaseFragment extends Fragment {
 
     protected final String TAG = this.getClass().getSimpleName();
@@ -18,12 +14,6 @@ public class BaseFragment extends Fragment {
 
     protected ActionBar getSupportActionBar() {
         return getBaseActivity().getSupportActionBar();
-    }
-
-    @Override public void onDestroyView() {
-        super.onDestroyView();
-        RefWatcher refWatcher = App.getRefWatcher(getActivity());
-        refWatcher.watch(this);
     }
 
 }
